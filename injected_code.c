9618aa2e551199e581aa7e74f5eb247197e98043
Max <maxpet@tutanota.com>
2022-08-22 15:45:08 -0400

    Edit machine code in 6 places to increase turn metalimit

--- injected_code.c
+++ injected_code.c
@@ -1342,6 +1342,14 @@ apply_machine_code_edits (struct c3x_config const * cfg)
 	WITH_MEM_PROTECTION (ADDR_RESOURCE_TILE_COUNT_MASK, 1, PAGE_EXECUTE_READWRITE) {
 		*(byte *)ADDR_RESOURCE_TILE_COUNT_MASK = (cfg->count_mills > 0) ? 0xFF : 0x00;
 	}
+
+	byte * addr_turn_metalimits[] = {ADDR_TURN_METALIMIT_1, ADDR_TURN_METALIMIT_2, ADDR_TURN_METALIMIT_3, ADDR_TURN_METALIMIT_4, ADDR_TURN_METALIMIT_5, ADDR_TURN_METALIMIT_6};
+	for (int n = 0; n < (sizeof addr_turn_metalimits) / (sizeof addr_turn_metalimits[0]); n++) {
+		byte * addr = addr_turn_metalimits[n];
+		WITH_MEM_PROTECTION (addr, 4, PAGE_EXECUTE_READWRITE) {
+			int_to_bytes (addr, 1000000);
+		}
+	}
 }
 
 void
