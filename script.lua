23e0df012c98929701e01225d3bc02e9598a34b1
Max <maxpet@tutanota.com>
2022-10-01 15:05:44 -0400

    Perform actual happiness check in Lua

--- script.lua
+++ script.lua
@@ -2,14 +2,25 @@
 local civ3 = require("civ3")
 
 function InterceptEndOfTurn()
-  local unhappyCount = 0
+  local numUnhappyCities, firstUnhappyCity = 0, nil
   for city in civ3.GetUIController():Cities() do
+    -- recompute happiness
+    local numHappy, numUnhappy = 0, 0
     for citizen in city:Citizens() do
-      if citizen.Mood == civ3.CitizenMood.Unhappy then
-        unhappyCount = unhappyCount + 1
+      if citizen.Mood == civ3.CitizenMood.Happy then
+        numHappy = numHappy + 1
       end
+      if citizen.Mood == civ3.CitizenMood.Unhappy then
+        numUnhappy = numUnhappy + 1
+      end
+    end
+    if numUnhappy > numHappy then
+      numUnhappyCities = numUnhappyCities + 1
+      if firstUnhappyCity == nil then firstUnhappyCity = city end
     end
   end
 
-  civ3.PopUpInGameError("Unhappy count: " .. unhappyCount);
+  if firstUnhappyCity ~= nil then
+    civ3.PopUpInGameError("At least one city is unhappy")
+  end
 end
