f98a98624e291e59b7663d194e6cd78bcc9952d2
Max <maxpet@tutanota.com>
2023-04-13 16:02:53 -0400

    Add ByteLayer

--- Trade Net X/TradeNetX.cpp
+++ Trade Net X/TradeNetX.cpp
@@ -1,5 +1,7 @@
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <tuple>
 
 #define NOVIRTUALKEYCODES // Keycodes defined in Civ3Conquests.h instead
 #include "windows.h"
@@ -40,6 +42,62 @@ has_road_open_to (Tile * tile, int civ_id)
 		 (tile->Territory_OwnerID == 0) ||
 		 ! leaders[civ_id].At_War[tile->Territory_OwnerID]);
 }
+
+struct ByteBlock {
+	byte tiles[64];
+};
+
+// ByteLayer stores one byte of data per map tile in a cache-efficient way, assuming that when you access the data for one tile you'll soon need it
+// for the surrounding tiles too.
+class ByteLayer
+{
+public:
+	ByteBlock * blocks;
+	int map_width, map_height;
+	int width_in_blocks, height_in_blocks;
+
+	ByteLayer (int _map_width, int _map_height)
+	{
+		map_width = _map_width, map_height = _map_height;
+		width_in_blocks  = map_width  / 16 + (map_width  % 16 != 0 ? 1 : 0);
+		height_in_blocks = map_height / 8  + (map_height % 8  != 0 ? 1 : 0);
+		blocks = (ByteBlock *)calloc (width_in_blocks * height_in_blocks, sizeof *blocks);
+	}
+
+	~ByteLayer ()
+	{
+		free (blocks);
+	}
+
+	std::tuple<int, int>
+	compute_location (int x, int y)
+	{
+		// Coordinates of the block (x, y) is in and the "remainder" coords inside the block
+		int block_x = x / 16, r_x = x % 16,
+		    block_y = y / 8 , r_y = y % 16;
+
+		int block_index = block_y * width_in_blocks + block_x,
+		    tile_index = r_y * 8 + (r_y%2 == 0 ? r_x : r_x-1) / 2;
+
+		return {block_index, tile_index};
+	}
+
+	byte
+	get (int x, int y)
+	{
+		int block_index, tile_index;
+		std::tie(block_index, tile_index) = compute_location (x, y);
+		return blocks[block_index].tiles[tile_index];
+	}
+
+	void
+	set (int x, int y, byte val)
+	{
+		int block_index, tile_index;
+		std::tie(block_index, tile_index) = compute_location (x, y);
+		blocks[block_index].tiles[tile_index] = val;
+	}
+};
 
 EXPORT_PROC
 void
