57a1c70deb95ed0725ea997dc9f9c458a0175cc1
Max <maxpet@tutanota.com>
2023-11-14 16:00:50 -0500

    Include some general info about Trade Net X; Add Trade Net X files to the release assembler script

--- assemble_release.bat
+++ assemble_release.bat
@@ -2,5 +2,5 @@
 echo !!!
 echo !!! Remember to update the version numbers in three places: changelog, C3X.h, README
 echo !!!
-7z.exe a NEW_RELEASE.zip Art lend tcc Text C3X.h changelog.txt civ_prog_objects.csv Civ3Conquests.h default.c3x_config.ini ep.c injected_code.c common.c INSTALL.bat README.txt RUN.bat
+7z.exe a NEW_RELEASE.zip Art lend tcc Text C3X.h changelog.txt civ_prog_objects.csv Civ3Conquests.h default.c3x_config.ini ep.c injected_code.c common.c INSTALL.bat README.txt RUN.bat "Trade Net X/TradeNetX.dll" "Trade Net X/TradeNetX.cpp" "Trade Net X/Civ3Conquests.hpp" "Trade Net X/BUILD.bat" "Trade Net X/TRADE NET X INFO.txt"
 pause
