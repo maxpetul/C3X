3bf771dbfd4822c468f7386d91a786d32928736c
Max <maxpet@tutanota.com>
2025-07-30 21:35:51 -0400

    Include trade_net_refs.txt in releases

--- assemble_release.bat
+++ assemble_release.bat
@@ -3,5 +3,5 @@ echo !!!
 echo !!! Remember to update the version numbers in FOUR places: changelog, C3X.h, README, default config
 echo !!! If the AMB Editor was updated, its about message must be changed to this version
 echo !!!
-7z.exe a NEW_RELEASE.zip Art lend tcc Text C3X.h changelog.txt civ_prog_objects.csv Civ3Conquests.h default.c3x_config.ini ep.c injected_code.c common.c INSTALL.bat README.txt RUN.bat "Trade Net X/TradeNetX.dll" "Trade Net X/TradeNetX.cpp" "Trade Net X/Civ3Conquests.hpp" "Trade Net X/BUILD.bat" "Trade Net X/TRADE NET X INFO.txt" "AMB Editor/*.c" "AMB Editor/*.h" "AMB Editor/RUN.bat" "AMB Editor/README.txt" "AMB Editor/amb_editor.ini"
+7z.exe a NEW_RELEASE.zip Art lend tcc Text C3X.h changelog.txt civ_prog_objects.csv trade_net_refs.txt Civ3Conquests.h default.c3x_config.ini ep.c injected_code.c common.c INSTALL.bat README.txt RUN.bat "Trade Net X/TradeNetX.dll" "Trade Net X/TradeNetX.cpp" "Trade Net X/Civ3Conquests.hpp" "Trade Net X/BUILD.bat" "Trade Net X/TRADE NET X INFO.txt" "AMB Editor/*.c" "AMB Editor/*.h" "AMB Editor/RUN.bat" "AMB Editor/README.txt" "AMB Editor/amb_editor.ini"
 pause
