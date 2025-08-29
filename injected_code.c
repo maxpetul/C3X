90eb61e0e1aef8d8376250ef04856042cd5db231
Max <maxpet@tutanota.com>
2025-08-28 21:23:10 -0400

    Update changelog; Comment out experimental minimap enlargement

--- injected_code.c
+++ injected_code.c
@@ -12067,8 +12067,10 @@ void __fastcall
 patch_Main_GUI_position_elements (Main_GUI * this)
 {
 	Main_GUI_position_elements (this);
-	this->Mini_Map_Click_Rect.top -= 105;
-	this->Mini_Map_Click_Rect.right += 229;
+
+	// Double size of minimap
+	// this->Mini_Map_Click_Rect.top -= 105;
+	// this->Mini_Map_Click_Rect.right += 229;
 }
 
 #define PEDIA_DESC_LINES_PER_PAGE 38
