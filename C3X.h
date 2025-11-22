2b7076c79eabd599295152871bb59341e4325b7d
Insta Fluff <instafluff0@gmail.com>
2025-11-22 08:20:06 -0800

    Add to config

--- C3X.h
+++ C3X.h
@@ -645,7 +645,7 @@ const struct district_config special_district_defaults[USED_SPECIAL_DISTRICT_TYP
 	{
 		.command = UCV_Build_Port, .name = "Port", .tooltip = "Build Port",
 		.advance_prereq = "Map Making", .allow_multiple = true, .vary_img_by_era = true, .vary_img_by_culture = false, .is_dynamic = false, .dependent_improvement_count = 2, .is_maritime = true,
-		.img_paths = {"Port_NW.pcx", "Port_N.pcx", "Port_NE.pcx", "Port_E.pcx", "Port_SE.pcx", "Port_S.pcx", "Port_SW.pcx", "Port_W.pcx"},
+		.img_paths = {0},// {"Port_NW.pcx", "Port_N.pcx", "Port_NE.pcx", "Port_E.pcx", "Port_SE.pcx", "Port_S.pcx", "Port_SW.pcx", "Port_W.pcx"},
 		.buildable_square_types_mask = DEFAULT_DISTRICT_BUILDABLE_MASK,
 		.img_path_count = 8, .max_building_index = 2, .btn_tile_sheet_column = 0, .btn_tile_sheet_row = 0,
 		.culture_bonus = 0, .science_bonus = 0, .food_bonus = 2, .gold_bonus = 2, .shield_bonus = 0, .defense_bonus_percent = 0
