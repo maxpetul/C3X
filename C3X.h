8f723e42f867f301fb72ec248e359e21404c658f
Instafluff <instafluff0@gmail.com>
2026-05-06 16:03:10 -0700

    Move do_not_place_barb_huts_or_camps_on_impassable_tiles to align with place it is declared upstream

--- C3X.h
+++ C3X.h
@@ -322,7 +322,7 @@ struct c3x_config {
 	bool allow_upgrades_in_any_city;
 	bool do_not_generate_volcanos;
 	bool do_not_pollute_impassable_tiles;
-	bool do_not_place_barbarian_huts_on_impassable_tiles;
+	bool do_not_place_barb_huts_or_camps_on_impassable_tiles;
 	bool show_hp_of_stealth_attack_options;
 	bool exclude_invisible_units_from_stealth_attack;
 	bool exclude_passengers_from_stealth_attack;
@@ -482,8 +482,6 @@ struct c3x_config {
 	int ai_auto_build_great_wall_strategy;
 
 	bool enable_city_work_radii_highlights;
-
-	bool do_not_place_barb_huts_or_camps_on_impassable_tiles;
 };
 
 enum stackable_command {
