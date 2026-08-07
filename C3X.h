2cfca855e4489b5dc3f9a3e7d5fb6fb09295fe4b
Max <maxpet@tutanota.com>
2026-08-06 22:15:42 -0400

    Properly merge 27C changes into changelog; Reset version numbers to 27

--- C3X.h
+++ C3X.h
@@ -9,7 +9,7 @@ typedef unsigned char byte;
 #define __fastcall __attribute__((fastcall))
 #include "Civ3Conquests.h"
 
-#define MOD_VERSION 2702
+#define MOD_VERSION 2700
 #define MOD_PREVIEW_VERSION 0
 
 #define COUNT_TILE_HIGHLIGHTS 11
